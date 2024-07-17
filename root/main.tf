a939cef3da69a9fa263c81f603e4869048a4b61c
cynthia Wobyeb <cynthia.wobyeb@digited.it.com>
2024-07-17 16:04:41 +0200

    new change

--- root/main.tf
+++ root/main.tf
@@ -5,23 +5,23 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "./vpc"
+  source = "../vpc"
 }
 
 module "ec2" {
-  source        = "./ec2"
+  source        = "../ec2"
   instance_type = "t2.nano" 
   instance_name = "product-search-instance"
   subnet_id     = module.vpc.subnet1_id
 }
 
 module "s3" {
-  source      = "./s3"
+  source      = "../s3"
   bucket_name = "amazon-images-bucket-093235"
 }
 
 module "ec2_new" {
-  source        = "./ec2"
+  source        = "../ec2"
   instance_name = "users-project-instance"
   subnet_id     = module.vpc.subnet1_id
 }
