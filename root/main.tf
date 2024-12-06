0c84124dcca8f78a153db440cb5f2541a59c2b61
cynthia091991 <136771400+cynthia091991@users.noreply.github.com>
2024-12-06 12:39:56 +0100

    Update main.tf

--- root/main.tf
+++ root/main.tf
@@ -1,7 +1,15 @@
+terraform  {
+required_providers{
+ aws = {
+source = "hashicorp/aws
+version = "~> 5.0"
+ }
+required_version = ">= 1.9.6"
+}
+}
 provider "aws" {
   region     = "eu-south-1"         # Change this to your desired region
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
+  default    = profile
 }
 
 module "vpc" {
